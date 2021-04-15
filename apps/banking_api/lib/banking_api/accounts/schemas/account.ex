910e39035b9f96994e61e81f0a66e3dc25d72319
Erick Patrick <80784084+ckoliveira@users.noreply.github.com>
2021-04-14 21:19:48 -0300

    Chore/model api entities (#2)
    * change config files and remove some files
    
    * model the entities of the api

---
+++ apps/banking_api/lib/banking_api/accounts/schemas/account.ex
@@ -0,0 +1,21 @@
+defmodule BankingApi.Accounts.Schemas.Account do
+  @moduledoc """
+  TBD
+  """
+
+  use Ecto.Schema
+
+  alias BankingApi.Users.Schemas.User
+
+  @primary_key {:id, :binary_id, autogenerate: true}
+  @foreign_key_type :binary_id
+  schema "accounts" do
+    belongs_to :user, User
+    field :balance, :integer
+
+    timestamps()
+  end
+
+
+
+end
