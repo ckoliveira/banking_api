d43fa03bee594746d15031ced51271cc28f451de
ckoliveira <erick.oliveira@stone.com.br>
2021-08-13 16:25:49 -0300

    chore: refactor seeds

--- apps/banking_api/priv/repo/seeds.exs
+++ apps/banking_api/priv/repo/seeds.exs
@@ -1,39 +1,31 @@
-# Script for populating the database. You can run it as:
-#
-#     mix run priv/repo/seeds.exs
-#
-# Inside the script, you can read and write to any of your
-# repositories directly:
-#
-#     BankingApi.Repo.insert!(%BankingApi.SomeSchema{})
-#
-# We recommend using the bang functions (`insert!`, `update!`
-# and so on) as they will fail if something goes wrong.
-
 defmodule BankingApi.Seeds do
   alias BankingApi.User
 
-  User.create(%{
-    name: "Fernanda Santos",
-    cpf: "11111111124",
-    password: "ad67ffg87d9"
-  })
+  users = [
+    %{
+      name: "Fernanda Santos",
+      cpf: "11111111124",
+      password: "ad67ffg87d9"
+    },
+    %{
+      name: "Ricardo Villalobos",
+      cpf: "11111111123",
+      password: "4f45t8fni"
+    },
+    %{
+      name: "Tom Jenkinson",
+      cpf: "11111111125",
+      password: "v48sib9d0"
+    },
+    %{
+      name: "Richard D James",
+      cpf: "11111111126",
+      password: "5gh8g994n"
+    }
+  ]
 
-  User.create(%{
-    name: "Ricardo Villalobos",
-    cpf: "11111111123",
-    password: "4f45t8fni"
-  })
-
-  User.create(%{
-    name: "Tom Jenkinson",
-    cpf: "11111111125",
-    password: "v48sib9d0"
-  })
-
-  User.create(%{
-    name: "Richard D James",
-    cpf: "11111111126",
-    password: "5gh8g994n"
-  })
+  Enum.map(users, fn %{name: name, cpf: cpf, password: password} ->
+    password_hash = Argon2.hash_pwd_salt(password)
+    User.create(%{name: name, cpf: cpf, password_hash: password_hash})
+  end)
 end
